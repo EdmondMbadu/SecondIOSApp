72df134616d9cef022bb88d07163f504c5c234b8
Edmond Mbadu <mbadungoma@gmail.com>
2019-10-24 08:37:24 -0400

    Initial Commit

---
+++ SecondIOSApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondIOSApp
+//
+//  Created by Mbadu, Edmond Ngoma on 10/24/19.
+//  Copyright © 2019 Mbadu, Edmond Ngoma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
